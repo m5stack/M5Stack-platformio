dce29dda1158a0013d3df6f48ae1c2abe9d8e8b3
0x1abin <270995079@qq.com>
2017-09-26 11:52:27 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <M5Stack.h>
+
+// the setup routine runs once when M5Stack starts up
+void setup(){
+  
+  // Initialize the M5Stack object
+  m5.begin();
+  
+  // LCD display
+  m5.Lcd.printf("hello world");
+
+}
+
+// the loop routine runs over and over again forever
+void loop() {
+
+  // more examples reference the /lib/M5Stack/examples
+}
